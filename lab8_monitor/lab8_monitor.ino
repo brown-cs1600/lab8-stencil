07372a1697274c61327d4c3feae99b228b49d09d
mziz <milda_zizyte@brown.edu>
2024-11-11 14:43:40 -0500

    update stencil and binaries

--- lab8_monitor/lab8_monitor.ino
+++ lab8_monitor/lab8_monitor.ino
@@ -8,7 +8,8 @@ void setup() {
   Serial1.setTimeout(10);
   pinMode(7, OUTPUT); // the status LED
   pinMode(3, INPUT);  // the AC LED
-  digitalWrite(7, HIGH); // the potentiometer
+  digitalWrite(7, HIGH);
+  analogReadResolution(10); // the potentiometer
 }
 
 void loop() {
@@ -84,7 +85,7 @@ void loop() {
   // transition the FSM
   if(doMonitor) {
     Serial.print("AC LED: ");
-    bool ledVal = digitalRead(4) == HIGH;
+    bool ledVal = digitalRead(3) == HIGH;
     Serial.println(ledVal);
     
     Serial.print("Potentiometer: ");
