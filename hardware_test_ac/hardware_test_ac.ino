fb8de3232687861e9fb3ad4d195e5b6df839ff52
mziz <milda_zizyte@brown.edu>
2024-11-11 13:25:04 -0500

    Update hardware_test_ac.ino

--- hardware_test_ac/hardware_test_ac.ino
+++ hardware_test_ac/hardware_test_ac.ino
@@ -1,8 +1,8 @@
-const int DES_TEMP = 6;
+const int DES_TEMP = 9;
 const int CUR_TEMP = 5;
 const int AC_OUT = 3;
 
-const int BUTTON = 4;
+const int BUTTON = 7;
 
 const int DIAL = A1;
 
@@ -11,6 +11,7 @@ void setup() {
   pinMode(AC_OUT, OUTPUT);
   pinMode(BUTTON, INPUT);
   analogWriteResolution(8);
+  analogReadResolution(10);
 
   Serial.begin(9600);
   while(!Serial);
@@ -37,7 +38,7 @@ void loop() {
     i++;
   } else if (i < 12) {
     analogWrite(DES_TEMP, 255);
-    analogWrite(CUR_TEM, 0);
+    analogWrite(CUR_TEMP, 0);
     digitalWrite(AC_OUT, HIGH);
     i++;
   } else {
